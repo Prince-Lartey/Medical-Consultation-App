57dd7d25971b17dcf2423107fb5e999e35b6ae68
Prince-Lartey <lartey.princ@gmail.com>
2025-02-21 23:53:55 +0000

    setting the project up...

--- src/app/(front)/layout.tsx
+++ src/app/(front)/layout.tsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/Frontend/Navbar'
 import React, { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
         <div>
+            <Navbar />
             {children}
         </div>
     )
