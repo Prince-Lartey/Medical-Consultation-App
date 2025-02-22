08cec0812f6257d71e507d548c222e1a565b28f1
Prince-Lartey <lartey.princ@gmail.com>
2025-02-22 12:12:45 +0000

    Done with the Hero section..

--- src/components/Frontend/Hero.tsx
+++ src/components/Frontend/Hero.tsx
@@ -120,12 +120,3 @@ import SearchBar from "@/components/Frontend/SearchBar";
 };
 
 export default Hero;
-
-const SingleImage = ({ href, imgSrc }: {href: string, imgSrc: string}) => {
-    return (
-        <a href={href} className="flex w-full items-center justify-center">
-            <img src={imgSrc} alt="brand image" className="h-10 w-full" />
-        </a>
-        
-    );
-};
