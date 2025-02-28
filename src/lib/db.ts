fbe9446cc8a1d380d9b2f96e0cd06ab0accfa323
Prince-Lartey <lartey.princ@gmail.com>
2025-02-28 20:37:44 +0000

    minor changes..

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-    const prisma: PrismaClient | undefined;
+    let prisma: PrismaClient | undefined;
 }
 
-export const prismaClient = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismaClient;
+export const prismaClient = (globalThis as any).prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") (globalThis as any).prisma = prismaClient;
