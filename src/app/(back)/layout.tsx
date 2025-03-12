ff6a220956817778b284bea4babb613a19976b23
Prince-Lartey <lartey.princ@gmail.com>
2025-03-12 10:31:55 +0000

    making changes to the doctors dashboard..

--- src/app/(back)/layout.tsx
+++ src/app/(back)/layout.tsx
@@ -1,11 +1,17 @@
-// import Navbar from '@/components/Dashboard/Navbar'
-// import Sidebar from '@/components/Dashboard/Sidebar'
+import Navbar from '@/components/Dashboard/Navbar'
+import Sidebar from '@/components/Dashboard/Sidebar'
 import React, { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <div>
-            <div className="p-8">{children}</div>
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <Sidebar />
+            <div className="flex flex-col">
+                <Navbar />
+                <div className="p-4">
+                    {children}
+                </div>
+            </div>
         </div>
     )
 }
