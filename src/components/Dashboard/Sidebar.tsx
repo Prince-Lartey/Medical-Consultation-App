220a1c73b07f4925f47eff088b839d5a3a38f6fb
Prince-Lartey <lartey.princ@gmail.com>
2025-03-12 17:16:59 +0000

    made the sidebar responsive

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -1,4 +1,6 @@
-import { Bell, Home, LineChart, Package, Package2, ShoppingCart, Users } from "lucide-react";
+"use client"
+
+import { Bell, Home, LineChart, Package, Package2, Settings, ShoppingCart, Users } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
@@ -10,8 +12,46 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
-export default async function Sidebar() {
+export default function Sidebar() {
+    const pathname = usePathname()
+
+    const sideBarLinks = [
+        {
+            name: "Dashboard",
+            path: "/dashboard",
+            icon: Home,
+        },
+        {
+            name: "Products",
+            path: "/dashboard/appointments",
+            icon: Package,
+        },
+        {
+            name: "Orders",
+            path: "/dashboard/orders",
+            icon: ShoppingCart,
+            badgeCount: 6,
+        },
+        {
+            name: "Customers",
+            path: "/dashboard/customers",
+            icon: Users,
+        },
+        {
+            name: "Analytics",
+            path: "/dashboard/analytics",
+            icon: LineChart,
+        },
+        {
+            name: "Settings",
+            path: "/dashboard/settings",
+            icon: Settings,
+        },
+    ]
+
     return (
         <div className="hidden border-r bg-muted/40 md:block">
                 <div className="flex h-full max-h-screen flex-col gap-2">
@@ -27,44 +67,22 @@ export default async function Sidebar() {
                     </div>
                     <div className="flex-1">
                         <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                            >
-                                <Home className="h-4 w-4" />
-                                Dashboard
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                            >
-                                <ShoppingCart className="h-4 w-4" />
-                                Orders
-                                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                6
-                                </Badge>
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                            >
-                                <Package className="h-4 w-4" />
-                                Products{" "}
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                            >
-                                <Users className="h-4 w-4" />
-                                Customers
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                            >
-                                <LineChart className="h-4 w-4" />
-                                Analytics
-                            </Link>
+                            {
+                                sideBarLinks.map((item, index) => {
+                                    const Icon = item.icon
+                                    return (
+                                        <Link key={index} href={item.path} className={cn("flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary", pathname === item.path ? "bg-muted text-primary" : "")}>
+                                            <Icon className="h-4 w-4" />
+                                            {item.name}
+                                            {item.badgeCount && (
+                                                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                                                    {item.badgeCount}
+                                                </Badge>
+                                            )}
+                                        </Link>
+                                    )
+                                })
+                            }
                         </nav>
                     </div>
                     <div className="mt-auto p-4">
