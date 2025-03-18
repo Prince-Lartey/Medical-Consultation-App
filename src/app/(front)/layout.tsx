5c07364939c09210fe22fbe2a666312e6669df83
Prince-Lartey <lartey.princ@gmail.com>
2025-03-18 14:17:07 +0000

    Made the home landing page protected

--- src/app/(front)/layout.tsx
+++ src/app/(front)/layout.tsx
@@ -1,11 +1,15 @@
 import Footer from '@/components/Frontend/Footer'
 import { SiteHeader } from '@/components/site-header'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 import React, { ReactNode } from 'react'
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+    const session = await getServerSession(authOptions)
+
     return (
         <div className="">
-            <SiteHeader />
+            <SiteHeader session={session}/>
             {children}
             <Footer />
         </div>
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,14 +1,38 @@
+"use client"
 
 import Link from "next/link"
 import { MobileNav } from "@/components/mobile-nav"
 import { Button } from "@/components/ui/button"
 // import { siteConfig } from "../../config/site"
 import ModeToggle from "./ModeToggle"
-
 import { MainNav } from "./main-nav"
 import { CommandMenu } from "./command-menu"
+import { Session } from "next-auth"
+import { useRouter } from "next/navigation"
+import { signOut } from "next-auth/react"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
 
-export function SiteHeader() {
+export function SiteHeader({session}: {session: Session | null}) {
+    const user = session?.user
+    const router = useRouter()
+
+    async function handleLogout() {
+        await signOut()
+        router.push("/login")
+    }
+
     return (
         <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="flex h-14 items-center gap-2 md:gap-4 px-10">
@@ -19,11 +43,36 @@ export function SiteHeader() {
                     <CommandMenu />
                 </div>
                 <nav className="flex items-center gap-4">
-                    <Button>
-                        <Link href="/login">
-                            Login
-                        </Link>
-                    </Button>
+                    {session && session.user && user?.email ? (
+                        <DropdownMenu >
+                            <DropdownMenuTrigger asChild>
+                                <Avatar className="cursor-pointer">
+                                    {user.image ? (
+                                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                    ) : (
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    )}
+                                </Avatar>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
+                                <DropdownMenuLabel className="text-sm text-gray-500">{user.email}</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <Link href="/dashboard">Dashboard</Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem onClick={() => handleLogout()} className="cursor-pointer">Logout</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    ) : (
+                        <Button>
+                            <Link href="/login">
+                                Login
+                            </Link>
+                        </Button>
+                    )}
+
                     <ModeToggle />
                 </nav>
                 </div>
