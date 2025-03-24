9b8a33054af6b8594e27446dbcc5b3de55ef2120
Prince-Lartey <lartey.princ@gmail.com>
2025-03-24 15:25:02 +0000

    ..

--- src/app/(front)/onboarding/[id]/page.tsx
+++ src/app/(front)/onboarding/[id]/page.tsx
@@ -8,7 +8,7 @@ export default async function page({params} : {params: Promise<{ id: string }>;}
         <div className="bg-cyan-800 dark:bg-slate-800">
             <div className="max-w-5xl mx-auto py-8 min-h-screen">
                 <h2>Welcome Doctor - {id}</h2>
-                <OnboardingSteps />
+                <OnboardingSteps id={id}/>
             </div>
         </div>
     )
--- src/components/Onboarding/OnboardingSteps.tsx
+++ src/components/Onboarding/OnboardingSteps.tsx
@@ -1,56 +1,75 @@
+"use client"
+
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
+import BioDataForm from './BioDataForm'
+import ContactInfo from './ContactInfo'
+import ProfessionInfo from './ProfessionInfo'
 
-export default function OnboardingSteps() {
+export default function OnboardingSteps({id}: {id: string}) {
+    const params = useSearchParams()
+    const page = params.get('page')
+
     const steps = [
         {
             title: 'Bio Data',
             page: 'bio-data',
+            component: <BioDataForm />,
         },
         {
             title: 'Contact Information',
             page: 'contact',
+            component: <ContactInfo />,
         },
         {
             title: 'Profession Information',
             page: 'profession',
+            component: <ProfessionInfo />
         },
         {
             title: 'Education Information',
             page: 'education',
+            component: <></>
         },
         {
             title: 'Practice Information',
             page: 'practice',
+            component: <></>
         },
         {
             title: 'Additional Information',
             page: 'additional',
+            component: <></>
         },
         {
             title: 'Availability',
             page: 'availability',
+            component: <></>
         },
     ]
+    const currentStep = steps.find((step) => step.page === page)
+
     return (
         <div className="grid grid-cols-12 mx-auto rounded-sm shadow-inner overflow-hidden border border-slate-200 min-h-screen bg-gray-100">
             <div className='col-span-full sm:col-span-3 divide-y-2 divide-gray-200'>
-                <Link href="#" className="block py-3 px-4 bg-cyan-900 text-gray-100 shadow-inner uppercase text-sm">Contact Information</Link>
                 {
                     steps.map((step, index) => {
                         return (
-                            <Link key={index} href="#" className={cn("block py-3 px-4 bg-gray-400 text-gray-800 shadow-inner text-sm uppercase", step.page === page ? "bg-cyan-900 text-gray-100" : "")}>{step.title}</Link>
+                            <Link 
+                                key={index} 
+                                href={`/onboarding/${id}?page=${step.page}`}
+                                className={cn("block py-3 px-4 bg-gray-300 text-gray-800 shadow-inner text-sm uppercase", step.page === page ? "bg-cyan-900 text-gray-100" : "")}
+                            >
+                                {step.title}
+                            </Link>
                         )
                     })
                 }
             </div>
             <div className='col-span-full sm:col-span-9 bg-gray-100 p-4'>
-                <div>2</div>
-                <div>2</div>
-                <div>2</div>
-                <div>2</div>
-                <div>2</div>
+                {currentStep?.component}
             </div>
         </div>
     )
