99e7a19b21568104b9f40fa4b46d42f99d7885bf
Prince-Lartey <lartey.princ@gmail.com>
2025-04-28 14:59:38 +0000

    update services

--- src/components/Dashboard/Doctor/DoctorServiceSettings.tsx
+++ src/components/Dashboard/Doctor/DoctorServiceSettings.tsx
@@ -1,57 +1,27 @@
 import React from 'react'
-import { Button } from "@/components/ui/button"
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+    Card
 } from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Input } from "@/components/ui/input"
 import UpdateServiceForm from './UpdateServiceForm'
 import { getServices } from '../../../../actions/services'
 import { getSpecialties } from '../../../../actions/specialties'
 import { getSymptoms } from '../../../../actions/symptoms'
-import { SelectOption } from '@/components/FormInputs/SelectInput'
 import { DoctorProfile } from '@prisma/client'
 
-export default async function DoctorServiceSettings({profile}: {profile: DoctorProfile | undefined | null}) {
-    const allServices = (await getServices()).data
-    const allSpecialties = (await getSpecialties()).data
-    const allSymptoms = (await getSymptoms()).data
-
-    const services: SelectOption[] = allServices?.map((service: any) => {
-        return {
-            label: service.title,
-            value: service.id,
-        }
-    }) || []
-
-    const specialties: SelectOption[] = allSpecialties?.map((specialty: any) => {
-        return {
-            label: specialty.title,
-            value: specialty.id,
-        }
-    }) || []
-
-    const symptoms: SelectOption[] = allSymptoms?.map((symptom: any) => {
-        return {
-            label: symptom.title,
-            value: symptom.id,
-        }
-    }) || []
+export default async function DoctorServiceSettings({profile}: {profile: DoctorProfile}) {
+    const services = (await getServices()).data
+    const specialties = (await getSpecialties()).data
+    const symptoms = (await getSymptoms()).data
 
     return (
         <div className="grid gap-6">
             <Card x-chunk="dashboard-04-chunk-1">
-                <CardHeader>
+                {/* <CardHeader>
                     <CardTitle>Choose Service</CardTitle>
                     <CardDescription>
                         Used to identify your store in the marketplace.
                     </CardDescription>
-                </CardHeader>
+                </CardHeader> */}
                 <UpdateServiceForm services={services} specialties={specialties} symptoms={symptoms} profile={profile}/>
                 
             </Card>
--- src/components/Dashboard/Doctor/UpdateServiceForm.tsx
+++ src/components/Dashboard/Doctor/UpdateServiceForm.tsx
@@ -5,36 +5,70 @@ import { SelectOption } from '@/components/FormInputs/SelectInput'
 import ShadSelectInput, { SelectOptions } from '@/components/FormInputs/ShadSelectInput'
 import { Button } from '@/components/ui/button'
 import { CardContent, CardFooter } from '@/components/ui/card'
-import { DoctorProfile } from '@prisma/client'
+import { DoctorProfile, Service, Specialty, Symptom } from '@prisma/client'
 import { Loader } from 'lucide-react'
 // import { useSession } from 'next-auth/react'
 import React, { useState } from 'react'
 import { updateDoctorProfile } from '../../../../actions/onboarding'
 import toast from 'react-hot-toast'
 import { updateDoctorService } from '../../../../actions/services'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
 
-export default function UpdateServiceForm({services, specialties, symptoms, profile}: {services: SelectOptions[], specialties: SelectOptions[], symptoms: SelectOptions[], profile: DoctorProfile}) {
+export default function UpdateServiceForm({services, specialties, symptoms, profile}: {services: Service[], specialties: Specialty[], symptoms: Symptom[], profile: DoctorProfile}) {
     // const {data: session, status} = useSession()
     // const user = session?.user
 
     const profileId = profile?.id
 
-    const [selectedServiceId, setSelectedServiceId] = useState()
-    const [specialtyId, setSpecialtyId] = useState()
-    const [symptomIds, setSymptomIds] = useState<SelectOption[]>([])
-    const [loading, setLoading] = useState(false)
+    const [selectedServiceId, setSelectedServiceId] = useState(profile?.serviceId)
+    const [specialtyId, setSpecialtyId] = useState(profile?.specialtyId)
+    const [symptomIds, setSymptomIds] = useState<string[]>(profile?.symptomIds || [])
+
+    const [savingServices, setSavingServices] = useState(false)
+    const [savingSpecialty, setSavingSpecialty] = useState(false)
+    const [savingSymptoms, setSavingSymptoms] = useState(false)
 
     async function handleUpdateService() {
-        setLoading(true)
+        setSavingServices(true)
         const data = {
             serviceId: selectedServiceId,
-            specialtyId,
-            symptomIds: symptomIds.map((symptom) => symptom.value),
         }
         try{
             await updateDoctorService(profileId, data)
-            setLoading(false)
+            setSavingServices(false)
             toast.success("Services updated successfully")
+        }catch (error){
+            console.error("Error updating doctor profile:", error)
+
+        }
+        console.log(data)
+    }
+
+    async function handleUpdateSpecialty() {
+        setSavingSpecialty(true)
+        const data = {
+            specialtyId,
+        }
+        try{
+            await updateDoctorService(profileId, data)
+            setSavingSpecialty(false)
+            toast.success("Specialty updated successfully")
+        }catch (error){
+            console.error("Error updating doctor profile:", error)
+        }
+        console.log(data)
+    }
+
+    async function handleUpdateSymptoms() {
+        setSavingSymptoms(true)
+        const data = {
+            symptomIds
+        }
+        try{
+            await updateDoctorService(profileId, data)
+            setSavingSymptoms(false)
+            toast.success("Symptoms Updated successfully")
         }catch (error){
             console.error("Error updating doctor profile:", error)
         }
@@ -44,15 +78,68 @@ export default function UpdateServiceForm({services, specialties, symptoms, prof
     return (
         <div>
             <CardContent className="space-y-4">
-                <ShadSelectInput label="Select Service" optionTitle="Service" className="" options={services} selectedOptions={selectedServiceId} setSelectedOptions={setSelectedServiceId} />
-                <ShadSelectInput label="Select Specialty" optionTitle="Specialty" className="" options={specialties} selectedOptions={specialtyId} setSelectedOptions={setSpecialtyId} />
-                <CustomMultiSelect label="Select Symptom" optionTitle="Symptom" className="" options={symptoms} selectedOptions={symptomIds} setSelectedOptions={setSymptomIds} />
+                <div className="border shadow rounded-md p-4 mt-4">
+                    <div className="flex items-center justify-between border-b">
+                        <h2 className="scroll-m-20 text-xl font-semibold tracking-tight py-2 mb-3">Choose the Service you would want to Offer.</h2>
+                        <Button onClick={handleUpdateService} disabled={savingServices}>
+                            {savingServices ? <Loader className="mr-2 h-4 w-4 animate-spin" /> : "Update Service"}
+                        </Button>
+                    </div>
+                    <div className="grid grid-cols-4 gap-2 py-3">
+                        {
+                            services?.map((service) => {
+                                return (
+                                    <button key={service.id} className={cn("flex items-center justify-center flex-col py-3 px-3 border rounded-md cursor-pointer", selectedServiceId === service.id ? "border-slate-900 border-2 bg-slate-50 dark:bg-slate-950 dark:border-slate-50" : "")} onClick={() => setSelectedServiceId(service.id)}>
+                                        <Image src={service.imageUrl} alt={service.title} width={100} height={100} className="w-14 h-14" />
+                                        <p className="text-xs capitalize">{service.title}</p>
+                                    </button>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className="border shadow rounded-md p-4">
+                    <div className="flex items-center justify-between border-b">
+                        <h2 className="scroll-m-20 text-xl font-semibold tracking-tight py-3 mb-3">Choose your Specialty.</h2>
+                        <Button onClick={handleUpdateSpecialty} disabled={savingSpecialty}>
+                            {savingSpecialty ? <Loader className="mr-2 h-4 w-4 animate-spin" /> : "Update Specialty"}
+                        </Button>
+                    </div>                    
+                    <div className="grid grid-cols-4 gap-2 py-3">
+                        {
+                            specialties?.map((specialty) => {
+                                return (
+                                    <button key={specialty.id} className={cn("flex items-center justify-center flex-col py-3 px-3 border rounded-md cursor-pointer", specialtyId === specialty.id ? "border-slate-900 border-2 bg-slate-50 dark:bg-slate-950 dark:border-slate-50" : "")} onClick={() => setSpecialtyId(specialty.id)}>
+                                        <p className="text-xs capitalize">{specialty.title}</p>
+                                    </button>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className="border shadow rounded-md p-4">
+                    <div className="flex items-center justify-between border-b">
+                        <h2 className="scroll-m-20 text-xl font-semibold tracking-tight py-3 mb-3">Choose the Symptoms you would want to attend to.</h2>
+                        <Button onClick={handleUpdateSymptoms} disabled={savingSymptoms}>
+                            {savingSymptoms ? <Loader className="mr-2 h-4 w-4 animate-spin" /> : "Update Symptoms"}
+                        </Button>
+                    </div>                    
+                    <div className="grid grid-cols-4 gap-2 py-3">
+                        {
+                            symptoms?.map((symptom) => {
+                                return (
+                                    <button key={symptom.id} className={cn("flex items-center justify-center flex-col py-3 px-3 border rounded-md cursor-pointer", symptomIds === symptom.id ? "border-slate-900 border-2 bg-slate-50 dark:bg-slate-950 dark:border-slate-50" : "")} onClick={() => setSymptomIds((prev) => prev.includes(symptom.id) ? prev.filter((id) => id !== symptom.id) : [...prev, symptom.id])}>
+                                        <p className="text-xs capitalize">{symptom.title}</p>
+                                    </button>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+
             </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-                <Button onClick={handleUpdateService} disabled={loading}>
-                    {loading ? <Loader className="mr-2 h-4 w-4 animate-spin" /> : "Save"}
-                </Button>
-            </CardFooter>
         </div>
     )
 }
