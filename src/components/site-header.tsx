c26042a8e0b6a92c71c2b80562bb3c89260addeb
Prince-Lartey <lartey.princ@gmail.com>
2025-05-11 20:03:11 +0000

    search functionality to header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -24,6 +24,7 @@ import {
     AvatarImage,
 } from "@/components/ui/avatar"
 import { generateInitials } from "@/utils/generateInitials"
+import SearchBar from "./Frontend/SearchBar"
 
 export function SiteHeader({session}: {session: Session | null}) {
     const user = session?.user
@@ -41,8 +42,8 @@ export function SiteHeader({session}: {session: Session | null}) {
                 <MainNav />
                 <MobileNav />
                 <div className="ml-auto flex items-center gap-2 md:flex-1 md:justify-end">
-                <div className="hidden w-full flex-1 md:flex md:w-auto md:flex-none">
-                    {/* <CommandMenu /> */}
+                <div className="w-full flex-1 md:flex md:w-auto md:flex-none">
+                    <SearchBar />
                 </div>
                 <nav className="flex items-center gap-4">
                     {session && session.user && user?.email ? (
