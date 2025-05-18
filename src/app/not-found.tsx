f2628a2e8ce6543d443b7ed855eb9b6984e54ff4
Prince-Lartey <lartey.princ@gmail.com>
2025-05-18 18:43:45 +0000

    bug fixing..

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,16 @@
 "use client"
 
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function NotFound() {
     const pathname = usePathname()
+
+    useEffect(() => {
+        if (typeof document !== "undefined") {
+            document.title = "Not Found"
+        }
+    }, [])
 
     const navigations = [
         {
