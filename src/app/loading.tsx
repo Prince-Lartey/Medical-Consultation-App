a1462d8365803fd346340c44a6b1906c352f5f68
Prince Lartey <lartey.princ@gmail.com>
2025-05-22 12:14:41 +0000

    clear bug...

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,20 @@
 "use client"
 
-import { Player } from '@lottiefiles/react-lottie-player'
+import dynamic from 'next/dynamic'
 import healthLoader from '@/utils/healthLoader.json'
+
+// Dynamically import the Player component with no SSR
+const Player = dynamic(
+    () => import('@lottiefiles/react-lottie-player').then((mod) => ({ default: mod.Player })),
+    { 
+        ssr: false,
+        loading: () => (
+            <div className="flex justify-center items-center h-screen">
+                <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-500"></div>
+            </div>
+        )
+    }
+)
 
 export default function Loader() {
     return (
